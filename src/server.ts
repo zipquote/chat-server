b8097bddf122359d18cbd001ca673e24c80a574c
Prakhar Shukla <prakhar.shukla321@gmail.com>
2024-10-05 16:57:17 +0530

    setup user session using express session package

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express, { Application } from 'express';
 import { createServer } from 'http';
 import { Server as SocketIOServer } from 'socket.io';
 import cors from 'cors';
+import RedisStore from "connect-redis";
+import session from 'express-session';
+import { createClient } from "redis";
 
 const app: Application = express();
 const server = createServer(app);
@@ -10,15 +13,32 @@ const io = new SocketIOServer(server, { cors: { origin: '*' } });
 app.use(cors());
 app.use(express.json());
 
-app.get('/heartbeat', (_, res) => {
-  res.send('Ok');
+const redisClient = createClient()
+redisClient.connect().then(() => {
+  console.log('CONNECTED TO REDIS SERVER');
+}).catch(console.error)
+
+const sessionStore = new RedisStore({ client: redisClient })
+
+app.use(session({
+  secret: "explodingcats",
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+  },
+}));
+
+app.get('/', (_, res) => {
+  res.status(200).send('<h1>Ok</h1>');
 });
 
 io.on('connection', (socket) => {
   console.log('New client connected');
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
